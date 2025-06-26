b1c49e17fbd63b41b2728deb79d404d5ec058362
Daniel <da.abarzua@duocuc.cl>
2025-06-25 21:43:05 -0400

    assemblers, controller, model, repository y services de Pago

--- src/main/java/com/pagos/pagos/model/PagoModel.java
+++ src/main/java/com/pagos/pagos/model/PagoModel.java
@@ -4,14 +4,11 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -26,18 +23,22 @@ public class PagoModel {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     
+     @Column(nullable = false)
     private BigDecimal monto;
+
+    @Column(nullable = false)
     private String estado;
+
+    @Column(name = "fecha_pago", nullable = false)
     private LocalDateTime fechaPago;
-    
-    @Column(name = "order_id")
+
+    @Column(name = "order_id", nullable = false)
     private UUID orderId;
-    
-    @Column(name = "usuario_id")
+
+    @Column(name = "usuario_id", nullable = false)
     private UUID usuarioId;
-    
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "transaction_id", referencedColumnName = "id")
-    private TransaccionModel transaccion;
+
+    @Column(name = "transaccion_id", nullable = false)
+    private UUID transaccionId;
 
 }
